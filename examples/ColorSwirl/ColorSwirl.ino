34ae60e81a08d05e79fe2ebd8248148a8d7b16f2
Matt Mets <matt.mets@cibomahto.com>
2013-12-08 21:34:52 +0800

    Remove RevB defines, add reset routines once serial disconnects, add (semi-broken) button example, update PatternPlayer example

--- examples/ColorSwirl/ColorSwirl.ino
+++ examples/ColorSwirl/ColorSwirl.ino
@@ -1,25 +1,21 @@
+
+
 #include <FastSPI_LED2.h>
 #include <Animation.h>
 
 #define LED_COUNT 60
 struct CRGB leds[LED_COUNT];
 
-#ifdef REVB // RevB boards have a slightly different pinout.
+#define LED_OUT       13
+#define BUTTON_IN     10
+#define ANALOG_INPUT  A9
+#define EXTRA_PIN_A    7
+#define EXTRA_PIN_B   11
 
-#define LED_OUT      5
-#define BUTTON_IN    13
-#define ANALOG_INPUT A11
-#define IO_A         15
-
-#else
-
-#define LED_OUT      13
-#define BUTTON_IN    10
-#define ANALOG_INPUT A9
-#define IO_A         7
-#define IO_B         11
-
-#endif
+#define BRIGHT_STEP_COUNT 5
+uint8_t brightnesSteps[BRIGHT_STEP_COUNT] = {5,15,40,70,93};
+uint8_t brightness = 4;
+uint8_t lastButtonState = 1;
 
 long last_time;
 
@@ -31,7 +27,12 @@ void setup()
   LEDS.showColor(CRGB(0, 0, 0));
   LEDS.setBrightness(93); // Limit max current draw to 1A
   LEDS.show();
-   
+
+  pinMode(BUTTON_IN, INPUT_PULLUP);
+  pinMode(ANALOG_INPUT, INPUT_PULLUP);
+  pinMode(EXTRA_PIN_A, INPUT_PULLUP);
+  pinMode(EXTRA_PIN_B, INPUT_PULLUP);
+  
   last_time = millis();
 }
 
@@ -71,10 +72,13 @@ void color_loop() {
 
 void serialLoop() {
   static int pixelIndex;
+  
+  unsigned long lastReceiveTime = millis();
 
   while(true) {
 
     if(Serial.available() > 2) {
+      lastReceiveTime = millis();
 
       uint8_t buffer[3]; // Buffer to store three incoming bytes used to compile a single LED color
 
@@ -96,15 +100,28 @@ void serialLoop() {
         }
       }
     }
+    
+    // If we haven't received data in 4 seconds, return to playing back our animation
+    if(millis() > lastReceiveTime + 4000) {
+      // TODO: Somehow the serial port gets trashed here, how to reset it?
+      return;
+    }
   }
 }
 
 void loop()
 {
   // If'n we get some data, switch to passthrough mode
-  if(Serial.available() > 0) {
+  if(Serial.available() > 2) {
     serialLoop();
   }
+  
+  uint8_t buttonState = digitalRead(BUTTON_IN);
+  if((buttonState != lastButtonState) && (buttonState == 0)) {
+    brightness = (brightness + 1) % BRIGHT_STEP_COUNT;
+    LEDS.setBrightness(brightnesSteps[brightness]);
+  }
+  lastButtonState = buttonState;
   
   color_loop();
 }
--- examples/PatternPlayer/PatternPlayer.ino
+++ examples/PatternPlayer/PatternPlayer.ino
@@ -1,27 +1,23 @@
 #include <FastSPI_LED2.h>
 #include <avr/pgmspace.h>
 #include <Animation.h>
-#include "pov.h"
+#include "animation.h"
 
 #define LED_COUNT 60
 struct CRGB leds[LED_COUNT];
 
-#ifdef REVB // RevB boards have a slightly different pinout.
-
-#define LED_OUT      5
-#define BUTTON_IN    13
-#define ANALOG_INPUT A11
-#define IO_A         15
-
-#else
-
-#define LED_OUT      13
+#define LED_OUT      13 
 #define BUTTON_IN    10
 #define ANALOG_INPUT A9
 #define IO_A         7
 #define IO_B         11
 
-#endif
+#define BRIGHT_STEP_COUNT 5
+uint8_t brightnesSteps[BRIGHT_STEP_COUNT] = {5,15,40,70,93};
+uint8_t brightness = 4;
+uint8_t lastButtonState = 1;
+
+int frameDelay = 30; // Number of ms each frame should be displayed.
 
 void setup()
 {  
@@ -29,16 +25,22 @@ void setup()
 
   LEDS.addLeds<WS2811, LED_OUT, GRB>(leds, LED_COUNT);
   LEDS.showColor(CRGB(0, 0, 0));
-  LEDS.setBrightness(93); // Limit max current draw to 1A
+  LEDS.setBrightness(brightnesSteps[brightness]);
   LEDS.show();
+  
+  pinMode(BUTTON_IN, INPUT_PULLUP);
 }
+
 
 void serialLoop() {
   static int pixelIndex;
+  
+  unsigned long lastReceiveTime = millis();
 
   while(true) {
 
     if(Serial.available() > 2) {
+      lastReceiveTime = millis();
 
       uint8_t buffer[3]; // Buffer to store three incoming bytes used to compile a single LED color
 
@@ -60,17 +62,32 @@ void serialLoop() {
         }
       }
     }
+    
+    // If we haven't received data in 4 seconds, return to playing back our animation
+    if(millis() > lastReceiveTime + 4000) {
+      // TODO: Somehow the serial port gets trashed here, how to reset it?
+      return;
+    }
   }
 }
 
 void loop()
 {
   // If'n we get some data, switch to passthrough mode
-  if(Serial.available() > 0) {
+  if(Serial.available() > 2) {
     serialLoop();
   }
   
-  pov.draw(leds);
-  delay(20);
+  // Check if the brightness should be adjusted
+  uint8_t buttonState = digitalRead(BUTTON_IN);
+  if((buttonState != lastButtonState) && (buttonState == 0)) {
+    brightness = (brightness + 1) % BRIGHT_STEP_COUNT;
+    LEDS.setBrightness(brightnesSteps[brightness]);
+  }
+  lastButtonState = buttonState;
+  
+  animation.draw(leds);
+  // TODO: More sophisticated wait loop to get constant framerate.
+  delay(frameDelay);
 }
 
